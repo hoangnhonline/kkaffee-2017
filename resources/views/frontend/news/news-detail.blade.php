0e03f34aeb999063ac5a45f252a941d7fc661953
HoangNH <hoangnhonline@gmail.com>
2017-10-07 07:57:02 +0700

    .

--- resources/views/frontend/news/news-detail.blade.php
+++ resources/views/frontend/news/news-detail.blade.php
@@ -3,64 +3,45 @@
 @include('frontend.partials.meta') 
 
 @section('content')
-<div class="block2 block-breadcrumb">
-  <div class="container">
-    <ul class="breadcrumb">
-      <li><a href="{{ route('home') }}" title="Trở về trang chủ">Trang chủ</a></li>            
-      <li class="active"><a href="{{ route('news-list', $cateDetail->slug ) }}" title="{!! $cateDetail->name !!}">{!! $cateDetail->name !!}</a></li>
-    </ul>
-  </div>
-</div><!-- /block-breadcrumb -->
-<div class="block block-two-col container">
-  <div class="row">
-    <div class="col-sm-9 col-xs-12 block-col-left">
-		<div class="block-title-commom block-dt-news">
-			<div class="block block-title">
-				<h2>
-					<i class="fa fa-home"></i>
-					{!! $cateDetail->name !!}
-				</h2>
-			</div>
-			<div class="block-content">
-				<div class="block block-article">
-					<h1 class="title">{!! $detail->title !!}</h1>
-					<div class="reviews-summary" id="rating-summary" itemscope itemtype="http://schema.org/Review">
-						
-                    </div><!-- /reviews-summary -->
-                    <div class="block-author">
-                    	<ul>
-                    		<li>
-                    			<span>Tác giả:</span>
-                    			<span class="name">{!! $detail->createdUser->display_name !!}</span>
-                    		</li>
-                    		<li>
-                    			{!! date('d/m/Y', strtotime($detail->created_at)) !!}
-                    		</li>
-                    		<li>
-                    			{!! Helper::view($detail->id, 2) !!} lượt xem
-                    		</li>
-                    	</ul>
-                    </div>
-					<div class="block-editor-content">{!! $detail->content !!}</div>
-					
-				</div>
-				<div class="block block-share" id="share-buttons">
-					<div class="share-item">
-						<div class="fb-like" data-href="{{ url()->current() }}" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>
-					</div>
-					<div class="share-item" style="max-width: 65px;">
-						<div class="g-plus" data-action="share"></div>
-					</div>
-					<div class="share-item">
-						<a class="twitter-share-button"
-					  href="https://twitter.com/intent/tweet?text={!! $detail->title !!}">
-					Tweet</a>
-					</div>
-					<div class="share-item">
-						<div class="addthis_inline_share_toolbox"></div>
-					</div>
-				</div><!-- /block-share-->		
-			    @if($tagSelected->count() > 0)
+<article class="mar-top40">
+    <div class="container">
+        <div class="breadcrumbs">
+            <ul>
+                <li><a href="{{ route('home') }}" title="Trở về trang chủ">Trang chủ</a></li>
+                <li><a href="{{ route('news-list', $cateDetail->slug ) }}" title="{!! $cateDetail->name !!}">{!! $cateDetail->name !!}</a></li>
+                <li>{!! $detail->title !!}</li>
+            </ul>
+        </div>
+    </div>
+    <section id="promotion-single" class="marg40">
+        <div class="container">
+            <div class="title-section">
+                {!! $detail->title !!}
+            </div>
+        </div>
+        <div class="container">
+        	
+            <div class="content-single">
+	            <div class="reviews-summary" id="rating-summary" itemscope itemtype="http://schema.org/Review">							
+	            </div><!-- /reviews-summary -->
+	            <div class="block-author">
+	            	<ul>
+	            		<li>
+	            			<span>Tác giả:</span>
+	            			<span class="name">{!! $detail->createdUser->display_name !!}</span>
+	            		</li>
+	            		<li>
+	            			{!! date('d/m/Y', strtotime($detail->created_at)) !!}
+	            		</li>
+	            		<li>
+	            			{!! Helper::view($detail->id, 2) !!} lượt xem
+	            		</li>
+	            	</ul>
+	            </div>
+	            <div class="clearfix"></div>
+                {!! $detail->content !!}
+                <div class="clearfix"></div>
+                @if($tagSelected->count() > 0)
 				<div class="block-tags">
 					<ul>
 						<li class="tags-first">Tags:</li>
@@ -72,12 +53,70 @@
 					</ul>
 				</div><!-- /block-tags -->
 				@endif
-			</div>
-		</div><!-- /block-ct-news -->
-	</div><!-- /block-col-left -->
-    @include('frontend.news.sidebar-detail')
-  </div>
-</div><!-- /block_big-title -->
+            </div>            
+            <div class="cart-info cart-side">
+              <div class="title-cart-info">THÔNG TIN GIỎ HÀNG</div>
+              <div class="content-cart-info">
+                  @if(!empty(Session::get('products')))
+                  <div class="list-items-cart">                        
+                      <?php $total = 0; ?>
+                      @if( $arrProductInfo->count() > 0)
+                          <?php $i = 0; ?>
+                        @foreach($arrProductInfo as $product)
+                        <?php 
+                        $i++;
+                        $price = $product->is_sale ? $product->price_sale : $product->price; 
+
+                        $total += $total_per_product = ($getlistProduct[$product->id]*$price);
+                        
+                        ?>
+                      <div class="item-cart">
+                          <div class="info-qty">
+                              <a class="qty-up" data-id="{{ $product->id }}" href="javascript:;"><i class="fa fa-plus-square" aria-hidden="true"></i></a>
+                              <input step="1" name="quantity" value="{{ $getlistProduct[$product->id] }}" class="qty-val">
+                              <a class="qty-down" data-id="{{ $product->id }}" href="javascript:;"><i class="fa fa-minus-square" aria-hidden="true"></i></a>
+                          </div>
+                          <p class="title-item">{!! $product->name !!}</p>
+                          <div class="price clearfix" style="font-size:14px">   
+                              <p class="pull-left" >{{ $getlistProduct[$product->id] }}x{{ number_format($price) }}</p>                             
+                              <p class="pull-right">{!! number_format($total_per_product) !!}đ</p>
+                          </div>
+                      </div>   
+                      
+                      @endforeach
+                      @endif                     
+                  </div>
+                  <ul class="">
+                      <li>
+                          <span class="pull-left cl_666">Cộng</span>
+                          <span class="pull-right cl_333">{!! number_format($total) !!}đ</span>
+                      </li>
+                      <!--<li>
+                          <span class="pull-left cl_ea0000">Giảm 30% tổng bill</span>
+                          <span class="pull-right cl_ea0000">66.000đ</span>
+                      </li>-->
+                      <li>
+                          <span class="pull-left cl_666">Phí phục vụ<br><small>(10% trên tổng đơn hàng)</small></span>
+                          <span class="pull-right cl_333">{{ number_format($total*10/100) }}đ</span>
+                      </li>
+                      <li class="bg_fffdee">
+                          <span class="pull-left cl_666">Tạm tính<br><small>(Giá chưa bao gồm COD)</small></span>
+                          <span class="pull-right cl_ea0000">{!! number_format($total + $total*10/100) !!}đ</span>
+                          <div class="clearfix"></div>
+                          <div class="action-cart ">
+                              <a href="{{ route('address-info') }}" class="btn btn-yellow">Đặt hàng</a>
+                              <a href="{{ route('empty-cart') }}" onclick="return confirm('Quý khách có chắc chắn bỏ hết hàng ra khỏi giỏ?'); " class="btn btn-defaultyellow">Xoá</a>
+                          </div>
+                      </li>
+                  </ul>
+                  @else
+                  <p class="cart-empty">Chưa có sản phẩm nào.</p>
+                  @endif
+              </div>
+          </div>
+        </div>
+    </section><!-- End product -->
+</article>
 <input type="hidden" id="rating-route" value="{{ route('rating') }}">
 <form id="rating-form">
 	{{ csrf_field() }}
