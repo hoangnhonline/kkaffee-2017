c6cc59e985794875f918aa79a7f8c8f657ce2550
HoangNH <mrhoang1989@gmail.com>
2017-11-28 16:25:46 +0700

    .

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -26,35 +26,6 @@
         </div>
         @endif
     </section>
-    <section id="welcome" class="marg40">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="title-section @if($isEdit) edit @endif" data-text="1" >{!! $textList[1] !!}</div>
-                    <p data-text="2" @if($isEdit) class="edit" @endif>{!! $textList[2] !!}</p>
-                </div>
-                <div class="col-md-6">
-                    <img src="{{ URL::asset('public/assets/img/hinh-gioi-thieu.png') }}" alt=""/>
-                </div>
-            </div>
-        </div>
-    </section><!-- End News -->
-    <?php 
-    $bannerArr = DB::table('banner')->where(['object_id' => 5, 'object_type' => 3])->orderBy('display_order', 'asc')->get();   
-    ?>
-    <?php $i = 0; ?>
-    @foreach($bannerArr as $banner)
-    <?php $i++; ?>
-    <section class="block-image marg40">
-    @if($banner->ads_url !='')
-    <a href="{{ $banner->ads_url }}" title="banner slide {{ $i }}">
-    @endif
-    <img src="{{ Helper::showImage($banner->image_url) }}" alt="banner slide {{ $i }}">
-    @if($banner->ads_url !='')
-    </a>
-    @endif
-    </section>
-    @endforeach    
     <section id="service-us" class="marg40">
         <div class="container">
             <div class="title-section text-center @if($isEdit) edit @endif" data-text="3">{!! $textList[3] !!}</div>
@@ -71,6 +42,38 @@
             @endforeach
         </div>
     </section><!-- End -->
+    <?php 
+    $bannerArr = DB::table('banner')->where(['object_id' => 5, 'object_type' => 3])->orderBy('display_order', 'asc')->get();   
+    ?>
+    <?php $i = 0; ?>
+    @foreach($bannerArr as $banner)
+    <?php $i++; ?>
+    <section class="block-image marg40">
+        <div class="container">
+            @if($banner->ads_url !='')
+            <a href="{{ $banner->ads_url }}" title="banner slide {{ $i }}">
+            @endif
+            <img src="{{ Helper::showImage($banner->image_url) }}" alt="banner slide {{ $i }}">
+            @if($banner->ads_url !='')
+            </a>
+            @endif
+        </div>
+    </section>
+    @endforeach
+    <section id="welcome" class="marg40">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="title-section @if($isEdit) edit @endif" data-text="1" >{!! $textList[1] !!}</div>
+                    <p data-text="2" @if($isEdit) class="edit" @endif>{!! $textList[2] !!}</p>
+                </div>
+                <div class="col-md-6">
+                    <iframe height="315" src="https://www.youtube.com/embed/{!! $settingArr['video_youtube_id'] !!}" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </section><!-- End News -->    
+    
 </article>
 @stop
 @section('js')
